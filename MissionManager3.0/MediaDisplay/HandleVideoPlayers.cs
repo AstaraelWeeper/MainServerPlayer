bde6869fad7e66b0a71513ec3edf8d4e464952a2
Rachel Griffiths <thisisrachelalice@hotmail.com>
2015-05-26 15:22:34 +0100

    rotate (hopefully) hooked up

--- MissionManager3.0/MediaDisplay/HandleVideoPlayers.cs
+++ MissionManager3.0/MediaDisplay/HandleVideoPlayers.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
+using System.Drawing;
 
 namespace SocketTutorial.FormsServer
 {
@@ -11,9 +12,15 @@ namespace SocketTutorial.FormsServer
         private VideoDisplay videoDisplay = null;
         private VideoDisplay videoDisplay2 = null;
         int screens = 2;
-        int resolutionWidth = 1920;
-        int resolutionHight = 1080;
-        int currentFrontDirection = 1; //1 = front, 2 = right, 3 = back, 4 = left
+       static  int resolutionWidth = 1920;
+        static int resolutionHight = 1080;
+        static int currentFrontDirection = 0; //0 = front, 1 = right, 2 = back, 3 = left
+        static int vid1X = (currentFrontDirection + 1) * resolutionWidth;
+        static int vid2X = (currentFrontDirection) * resolutionWidth;
+        static int y = 0;
+        Point videoPlayer1Location = new Point(vid1X,y);
+        Point videoPlayer2Location = new Point(vid2X,y);
+        
 
         public void initialisePlayers(string path)
         {
@@ -25,18 +32,80 @@ namespace SocketTutorial.FormsServer
                         videoDisplay2.Width = 0;
 
                         videoDisplay2.StartPosition = FormStartPosition.Manual;
-                      //  videoDisplay2 = videoDisplay.Width;
+                        videoDisplay2.Location = videoPlayer2Location;
                         videoDisplay.Show();
                         videoDisplay2.Show();
 
         }
 
-        private void rotateRight()
+        private string rotateRight()
         {
+            string stringReturnMessage;
+
+            if (currentFrontDirection < 3)
+            {
+                currentFrontDirection++;
+            }
+            else if (currentFrontDirection == 3)
+            {
+                currentFrontDirection = 0;
+            }
+            videoDisplay.Location = videoPlayer1Location;
+            videoDisplay2.Location = videoPlayer2Location;
+
+            stringReturnMessage = getFacingDirectionJSON();
+            return stringReturnMessage;
+            
         }
 
-        private void rotateLeft()
+        private string rotateLeft()
         {
+            string stringReturnMessage;
+
+            if (currentFrontDirection > 0)
+            {
+                currentFrontDirection--;
+            }
+            else if (currentFrontDirection == 0)
+            {
+                currentFrontDirection = 3;
+            }
+            videoDisplay.Location = videoPlayer1Location;
+            videoDisplay2.Location = videoPlayer2Location;
+
+            stringReturnMessage = getFacingDirectionJSON();
+            return stringReturnMessage;
+        }
+
+        private string getFacingDirectionJSON()
+        {
+            string stringReturnMessage;
+            if (currentFrontDirection == 0)
+            {
+                stringReturnMessage = "{\"messageType\":\"VideoPlayer\",\"messageBody\":\"Facing Front\"}";
+                return stringReturnMessage;
+            }
+            else if (currentFrontDirection == 1)
+            {
+                stringReturnMessage = "{\"messageType\":\"VideoPlayer\",\"messageBody\":\"Facing Right\"}";
+                return stringReturnMessage;
+            }
+            else if (currentFrontDirection == 2)
+            {
+                stringReturnMessage = "{\"messageType\":\"VideoPlayer\",\"messageBody\":\"Facing Back\"}";
+                return stringReturnMessage;
+            }
+            else if (currentFrontDirection == 3)
+            {
+                stringReturnMessage = "{\"messageType\":\"VideoPlayer\",\"messageBody\":\"Facing Left\"}";
+                return stringReturnMessage;
+            }
+
+            else
+            {
+                stringReturnMessage = "{\"messageType\":\"VideoPlayer\",\"messageBody\":\"Facing Calculation Failure\"}";
+                return stringReturnMessage;
+            }
         }
 
         public string GetVideoLength()
@@ -66,6 +135,18 @@ namespace SocketTutorial.FormsServer
                 {
                     stringReturnMessage = videoDisplay.Play();
                     videoDisplay2.Play();
+                    return stringReturnMessage;
+                }
+
+                else if (message.Contains("rotate right"))
+                {
+                    stringReturnMessage =  rotateRight();
+                     
+                    return stringReturnMessage;
+                }
+                else if (message.Contains("rotate left"))
+                {
+                    rotateLeft();
                     return stringReturnMessage;
                 }
 
--- MissionManager3.0/ParseJson.cs
+++ MissionManager3.0/ParseJson.cs
@@ -183,6 +183,7 @@ namespace SocketTutorial.FormsServer
                     paths += "{\"fileName\":\"" + fileNames[i] + "\",";
                     paths += "\"fileExtension\":\"" + fileExtensions[i] + "\",";
                     paths += "\"filePath\":\"" + filePaths[i] + "\"";
+                    paths += "\"fileSize\":\"" + fileSizes[i] + "\"";
 
                     paths += "},";
                 }
